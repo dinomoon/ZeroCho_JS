ebcaac58d37440b21b0b150bf39c8ad0f25f40af
MoonK <42693257+dinomoon@users.noreply.github.com>
2020-01-08 21:29:38 +0900

    Update app2.js

--- 10. 자스스톤/app2.js
+++ 10. 자스스톤/app2.js
@@ -1,207 +1,249 @@
+//리팩토링 한 후(기능: 카드 만들고 뽑고 턴넘기기 까지.. 공격X)
 window.addEventListener('load', function(){
-    var enemyDeck = document.querySelector('.enemy-deck');
-    var enemyFieldWrapper = document.querySelector('.enemy-field-wrapper');
-    var enemyHero = document.querySelector('.enemy-hero');
-    var enemyField = document.querySelector('.enemy-field');
-    var enemyCost = document.querySelector('.enemy-hero .cost');
-    var enemyDeckData = [];
-    var enemyFieldData = [];
-
-    var myDeck = document.querySelector('.my-deck');
-    var myFieldWrapper = document.querySelector('.my-field-wrapper');
-    var myField = document.querySelector('.my-field');
-    var myHero = document.querySelector('.my-hero');
-    var myCost = document.querySelector('.my-hero .cost');
-    var myDeckData = [];
-    var myFieldData = [];
-    var turnbtn = document.querySelector('.turn-btn');
-    var turn = true;
-
-    myFieldWrapper.style.backgroundColor = 'antiquewhite'
-    //NOTE
-    //턴 버튼 클릭했을 때
-    turnbtn.onclick = function(){
-        turn = !turn;
-        if(turn === false){ //상대방 턴으로 넘어갔을 때
-            myFieldData.forEach(function(card){
-                card.classList.remove('selected');
-                card.classList.remove('turn-over');
-            });
-            enemyFieldWrapper.style.backgroundColor = 'antiquewhite';
-            myFieldWrapper.style.backgroundColor = 'white';
-            enemyCost.textContent = 10;
-            // makeEnemyDeck(5);
-        } else { //내 턴으로 돌아왔을 때
-            enemyFieldData.forEach(function(card){
-                card.classList.remove('selected');
-                card.classList.remove('turn-over');
-            });
-            myFieldWrapper.style.backgroundColor = 'antiquewhite';
-            enemyFieldWrapper.style.backgroundColor = 'white'
-            myCost.textContent = 10;
-            // makeMyDeck(5);
-        }
+    var enemy = {
+        hero: document.querySelector('.enemy-hero'),
+        deck: document.querySelector('.enemy-deck'),
+        field: document.querySelector('.enemy-field'),
+        cost: document.querySelector('.enemy-field-wrapper .cost'),
+        deckData: [],
+        fieldData: [],
+        heroData: null,
+        selectCard: null,
+        selectCardData: null,
+    }
+    
+    var my = {
+        hero: document.querySelector('.my-hero'),
+        deck: document.querySelector('.my-deck'),
+        field: document.querySelector('.my-field'),
+        cost: document.querySelector('.my-field-wrapper .cost'),
+        deckData: [],
+        fieldData: [],
+        heroData: null,
+        selectCard: null,
+        selectCardData: null,
     }
 
-    //NOTE
-    //Card 생성자
-    function Card(hero){
+    var turn = true;
+    var turnBtn = document.querySelector('.turn-btn');
+    turnBtn.addEventListener('click', function(){
+        turn = !turn;
+        if(turn){ //내 턴으로 넘어왔을 때
+            my.cost.textContent = 10;
+            //턴 넘어갈 때 selected랑 turn-over 삭제
+            enemy.field.querySelectorAll('.card').forEach(function(card){
+                card.classList.remove('selected');
+                card.classList.remove('turn-over');
+            })
+            var hero = enemy.hero.querySelector('.card');
+            hero.classList.remove('selected');
+            hero.classList.remove('turn-over');
+            //턴이 넘어왔을 때, 덱에 카드를 5개로 채우기
+            var deckLength = my.deckData.length;
+            makeMyDeck(5-deckLength);
+        } else {
+            enemy.cost.textContent = 10;
+            //턴 넘어갈 때 selected랑 turn-over 삭제
+            my.field.querySelectorAll('.card').forEach(function(card){
+                card.classList.remove('selected');
+                card.classList.remove('turn-over');
+            })
+            var hero = my.hero.querySelector('.card');
+            hero.classList.remove('selected');
+            hero.classList.remove('turn-over');
+            //턴이 넘어왔을 때, 덱에 카드를 5개로 채우기
+            var deckLength = enemy.deckData.length;
+            makeEnemyDeck(5-deckLength);
+        }
+        document.querySelector('.enemy-field-wrapper').classList.toggle('turn');
+        document.querySelector('.my-field-wrapper').classList.toggle('turn');
+    })
+
+    function cardDomConect(data, dom, hero){
+        var template = document.querySelector('template');
+        var cloneNode = document.importNode(template.content.childNodes[1], true);
+        cloneNode.querySelector('.card-cost').textContent = data.cost;
+        cloneNode.querySelector('.card-attack').textContent = data.att;
+        cloneNode.querySelector('.card-hp').textContent = data.hp;
+        if(hero){
+            cloneNode.querySelector('.card-cost').style.display = 'none';
+            var name = document.createElement('div');
+            name.textContent = '영웅';
+            cloneNode.append(name);
+        }
+        cloneNode.addEventListener('click', function(e){
+            if(turn){ //내 턴일 때
+                if(!data.mine && my.selectCard && enemy.deckData.indexOf(data)===-1) {
+                    if(my.selectCardData.hero){
+                        my.heroData.hp -= data.att;
+                        my.hero.querySelector('.card-hp').textContent -= data.att;
+                    } else {
+                        var idx = my.fieldData.indexOf(my.selectCardData);
+                        my.fieldData[idx].hp -= data.att;
+                        if(my.fieldData[idx].hp <= 0){
+                            my.fieldData.splice(idx, 1);
+                            resetScreen(true);
+                        } else {
+                            var myCardHp = my.selectCard.querySelector('.card-hp');
+                            myCardHp.textContent -= data.att;
+                        }
+                    }
+                    
+                    data.hp -= my.selectCardData.att;
+                    if(data.hp <= 0){
+                        var idx = enemy.fieldData.indexOf(data);
+                        enemy.fieldData.splice(idx, 1);
+                    }
+                    resetScreen(false);
+                    my.selectCard.classList.remove('selected');
+                    my.selectCard.classList.add('turn-over');
+                    my.selectCard = null;
+                    my.selectCardData = null;
+                    return;
+                } else if (!data.mine) {
+                    return;
+                }
+                if(data.field){
+                    cloneNode.parentNode.parentNode.querySelectorAll('.card').forEach(function(card){
+                        card.classList.remove('selected');
+                    })
+                    cloneNode.classList.add('selected');
+                    my.selectCard = cloneNode;
+                    my.selectCardData = data;
+                } else {
+                    deckToField(data, turn);
+                }
+            } else { //상대방 턴일 때
+                if(data.mine && enemy.selectCard && my.deckData.indexOf(data)===-1){ //상대방 턴인 데 내 카드를 클릭했을 때
+                    if(enemy.selectCardData.hero){
+                        enemy.heroData.hp -= data.att;
+                        enemy.hero.querySelector('.card-hp').textContent -= data.att;
+                    } else {
+                        var idx = enemy.fieldData.indexOf(enemy.selectCardData);
+                        enemy.fieldData[idx].hp -= data.att;
+                        if(enemy.fieldData[idx].hp <= 0){
+                            enemy.fieldData.splice(idx, 1);
+                            resetScreen(false);
+                        } else {
+                            var enemyCardHp = enemy.selectCard.querySelector('.card-hp');
+                            enemyCardHp.textContent -= data.att;
+                        }
+                    }
+                
+                    data.hp -= enemy.selectCardData.att;
+                    if(data.hp <= 0){
+                        var idx = my.fieldData.indexOf(data);
+                        my.fieldData.splice(idx, 1);
+                    }
+                    resetScreen(true);
+
+                    enemy.selectCard.classList.remove('selected');
+                    enemy.selectCard.classList.add('turn-over');
+                    enemy.selectCard = null;
+                    enemy.selectCardData = null;
+                    return;
+                } else if(data.mine){
+                    return;
+                }
+                if(data.field){
+                    cloneNode.parentNode.parentNode.querySelectorAll('.card').forEach(function(card){
+                        card.classList.remove('selected');
+                    })
+                    cloneNode.classList.add('selected');
+                    enemy.selectCard = cloneNode;
+                    enemy.selectCardData = data;
+                } else {
+                    deckToField(data, turn);
+                }
+            }
+        })
+        dom.append(cloneNode);
+    }
+    function deckToField(data, myTurn){
+        var object = myTurn ? my : enemy;
+        var cost = Number(object.cost.textContent);
+        if(cost < data.cost){
+            return;
+        } else {
+            object.cost.textContent -= data.cost;
+        }
+        var idx = object.deckData.indexOf(data);
+        object.deckData.splice(idx, 1);
+        object.fieldData.push(data);
+        object.deck.innerHTML = '';
+        object.field.innerHTML = '';
+        object.fieldData.forEach(function(data){
+            cardDomConect(data, object.field);
+        });
+        object.deckData.forEach(function(data){
+            cardDomConect(data, object.deck);
+        })
+        data.field = true;
+    }
+    function resetScreen(where){
+        var object = where ? my : enemy;
+        object.hero.innerHTML = '';
+        cardDomConect(object.heroData, object.hero, true);
+
+        object.field.innerHTML = '';
+        object.fieldData.forEach(function(card){
+            cardDomConect(card, object.field, false);
+        })
+    }
+
+    function makeEnemyDeck(count){
+        for(var i=0; i<count; i++){
+            enemy.deckData.push(cardFactory(false, false));
+        }
+        enemy.deck.innerHTML = '';
+        enemy.deckData.forEach(function(card){
+            cardDomConect(card, enemy.deck, false);
+        });
+    }
+    function makeMyDeck(count){
+        for(var i=0; i<count; i++){
+            my.deckData.push(cardFactory(false, true));
+        }
+        my.deck.innerHTML = '';
+        my.deckData.forEach(function(card){
+            cardDomConect(card, my.deck, false);
+        });
+    }
+    function makeEnemyHero(){
+        enemy.heroData = cardFactory(true, false);
+        cardDomConect(enemy.heroData, enemy.hero, true);
+    }
+    function makeMyHero(){
+        my.heroData = cardFactory(true, true);
+        cardDomConect(my.heroData, my.hero, true);
+    }
+    function setting(){
+        makeEnemyDeck(5);
+        makeMyDeck(5);
+        makeEnemyHero();
+        makeMyHero();
+    }
+
+
+    function Card(hero, mine){
         if(hero){
             this.att = 1;
             this.hp = 30;
+            this.cost = 0;
+            this.field = true;
+            this.hero = true;
         } else {
-            this.att = Math.ceil(Math.random() * 5);
-            this.hp = Math.ceil(Math.random() * 5);
-            this.cost = Math.floor((this.att + this.hp) / 2);
+            this.att = Math.ceil(Math.random()*5);
+            this.hp = Math.ceil(Math.random()*5);
+            this.cost = Math.floor((this.att + this.hp)/2);
+        }
+        if(mine) {
+            this.mine = true
         }
     }
-
-    //NOTE 
-    //덱에 들어갈 카드랑 영웅 만들기
-    function makeMyDeck(count){
-        for(var i=0; i<count; i++){
-            myDeckData.push(new Card);
-        }
-        myDeckData.forEach(function(data){
-            cardDomConnect(data, myDeck);
-        })
-    }
-    function makeEnemyDeck(count){
-        for(var i=0; i<count; i++){
-            enemyDeckData.push(new Card);
-        }
-        enemyDeckData.forEach(function(data){
-            cardDomConnect(data, enemyDeck);
-        })
-    }
-    function makeMyHero(){
-        cardDomConnect(new Card(true), myHero, true);
-    }
-    function makeEnemyHero(){
-        cardDomConnect(new Card(true), enemyHero, true);
+    function cardFactory(hero, mine){
+        return new Card(hero, mine);
     }
 
-    makeMyHero();
-    makeEnemyHero();
-    makeMyDeck(5);
-    makeEnemyDeck(5);
-
-    //NOTE 
-    //만들어진 객체들을 화면에 나타내기
-    function cardDomConnect(data, where, hero){
-        var template = document.querySelector('template');
-        var cloneCard = document.importNode(template.content, true);
-        var info = cloneCard.querySelectorAll('span');
-        
-        if(hero){
-            info[0].style.display = 'none';
-            info[1].textContent = data.att;
-            info[2].textContent = data.hp;
-        } else {
-            info[0].textContent = data.cost;
-            info[1].textContent = data.att;
-            info[2].textContent = data.hp;
-        }
-        var card = cloneCard.childNodes[1];
-        if(where === myHero)
-            myFieldData.push(card);
-        else if(where === enemyHero)
-            enemyFieldData.push(card);
-        //NOTE 
-        //카드에 클릭 이벤트 붙이기
-        card.addEventListener('click', function(){
-            if(turn && where === myDeck && myCost.textContent >= data.cost && !(myFieldData.indexOf(card) > -1)){
-                //클릭하면 필드에 카드 넣기
-                myFieldData.push(card);
-                myField.append(card);
-                //필드에 들어간 카드는 덱 데이터에서 삭제하기
-                var idx = myDeckData.indexOf(card);
-                myDeckData.splice(idx, 1);
-                myCost.textContent -= data.cost;
-            }
-            else if (!turn && where === enemyDeck && enemyCost.textContent >= data.cost && !(enemyFieldData.indexOf(card) > -1)){
-                enemyFieldData.push(card);
-                enemyField.append(card);
-
-                var idx = enemyDeckData.indexOf(card);
-                enemyDeckData.splice(idx, 1);
-                enemyCost.textContent -= data.cost
-            }
-        });
-        console.log(myFieldData);
-        //NOTE 
-        //필드에 있는 카드들에 클릭 이벤트 붙이기
-        myFieldData.forEach(function(card){
-            card.addEventListener('click', function(){
-                myFieldData.forEach(function(card){
-                    card.classList.remove('selected');
-                })
-                if(turn){
-                    //내 필드에 있는 카드를 클릭했는 데,
-                    //내 턴이면 테두리 그려질 수 있게 selected 클래스 추가
-                    card.classList.add("selected");
-                } else {
-                    //내 필드에 있는 카드를 클릭했는 데,
-                    //내 턴이 아니면? 공격받은 거니까 체력을 깍기
-                    var selectedCard = document.querySelector('.selected'); //상대방이 선택한 카드
-                    var cardAtt = card.querySelector('.card-attack'); //상대방이 클릭한 내 카드의 공격력
-                    var cardHp = card.querySelector('.card-hp'); //상대방이 클릭한 내 카드의 체력
-                    var selectedCardAtt = selectedCard.querySelector('.card-attack'); //상대방이 선택한 카드의 공격력
-                    var selectedCardHp = selectedCard.querySelector('.card-hp'); //상대방이 선택한 카드의 체력
-                    
-                    console.log(cardHp);
-                    //NOTE 
-                    //한 번 공격한 카드들은 공격하지 못하게 하기
-                    if(!selectedCard.classList.contains('turn-over')){
-                        if(cardHp.textContent <= selectedCardAtt.textContent){
-                            selectedCardHp.textContent -= cardAtt.textContent;
-                            console.log(cardHp.textContent, selectedCardAtt.textContent);
-                            card.remove();
-                        }
-                        if (selectedCardHp.textContent <= cardAtt.textContent) {
-                            cardHp.textContent -= selectedCardAtt.textContent;
-                            selectedCard.remove();
-                        }
-                        selectedCard.classList.add('turn-over'); 
-                    } else {
-                        return;
-                    }
-                }
-            })
-        })
-
-        enemyFieldData.forEach(function(card){
-            card.addEventListener('click', function(){
-                enemyFieldData.forEach(function(card){
-                    card.classList.remove('selected');
-                })
-                if(!turn){
-                    card.classList.add("selected");
-                } else {
-                    var selectedCard = document.querySelector('.selected');
-                    var cardAtt = card.querySelector('.card-attack');
-                    var cardHp = card.querySelector('.card-hp');
-                    var selectedCardAtt = selectedCard.querySelector('.card-attack');
-                    var selectedCardHp = selectedCard.querySelector('.card-hp');
-                    console.log(cardHp.textContent);
-                    // console.log(cardHp, cardAtt);
-                    // console.log(cardHp <= cardAtt);
-                    if(!selectedCard.classList.contains('turn-over')){
-                        if(cardHp.textContent <= selectedCardAtt.textContent){
-                            selectedCardHp.textContent -= cardAtt.textContent;
-                            card.remove();
-                        }
-                        if (selectedCardHp.textContent <= cardAtt.textContent) {
-                            cardHp.textContent -= selectedCardAtt.textContent;
-                            selectedCard.remove();
-                        }
-                        selectedCard.classList.add('turn-over');
-                    } else {
-                        return;
-                    }
-                }
-            })
-        })
-        where.append(cloneCard);
-    }
+    setting();
 })
